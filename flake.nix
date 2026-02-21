17789ee8a3e7f4e481e7a53c1d38716ae4818516
Luis Quiñones <lpaandres2020@gmail.com>
2026-02-21 16:02:53 -0500

    feat: init

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A flake for the Raspberry Pi 5";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  };
+}
