6d6c975da0aa1120e956b23ee97c7b4ab010b22f
Luis Quiñones <lpaandres2020@gmail.com>
2026-03-09 21:12:07 -0500

    refactor: improve setup

--- flake.nix
+++ flake.nix
@@ -37,38 +37,37 @@
     home-manager,
     disko,
     ...
-  }: let
-    baseModules = [
-      {
-        imports = with nixos-raspberrypi.nixosModules; [
-          raspberry-pi-5.base
-          raspberry-pi-5.page-size-16k
-          raspberry-pi-5.display-vc4
-          ./config-txt.nix
-        ];
-      }
-      disko.nixosModules.disko
-      ./disko-usb-btrfs.nix
-      {
-        boot.tmp.useTmpfs = true;
-      }
-      home-manager.nixosModules.default
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.luisnquin = {
-          imports = [
-            black-terminal.homeModules.default
-            ./home.nix
-          ];
-        };
-      }
-      ./configuration.nix
-    ];
-  in {
-    nixosConfigurations.ryx = nixos-raspberrypi.lib.nixosSystemFull {
+  }: {
+    nixosConfigurations.ryx = nixos-raspberrypi.lib.nixosSystem {
       specialArgs = inputs // {inherit inputs;};
-      modules = baseModules;
+      modules = [
+        disko.nixosModules.disko
+        ./disko-usb-btrfs.nix
+        home-manager.nixosModules.default
+        {
+          imports = with nixos-raspberrypi.nixosModules; [
+            nixos-raspberrypi.lib.inject-overlays
+            nixos-raspberrypi.lib.inject-overlays-global
+            trusted-nix-caches
+            raspberry-pi-5.base
+            raspberry-pi-5.page-size-16k
+            raspberry-pi-5.display-vc4
+            ./config-txt.nix
+          ];
+
+          boot.tmp.useTmpfs = true;
+
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.luisnquin = {
+            imports = [
+              black-terminal.homeModules.default
+              ./home.nix
+            ];
+          };
+        }
+        ./configuration.nix
+      ];
     };
   };
 }
