ff5133f62f62df0178f5308659f68098f7406eef
Luis Quiñones <lpaandres2020@gmail.com>
2026-03-18 12:13:59 -0500

    feat: avahi setup

--- configuration.nix
+++ configuration.nix
@@ -18,6 +18,21 @@
   #   config.boot.kernelPackages.kernel.version
   # ];
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+      userServices = true;
+    };
+
+    denyInterfaces = ["docker0" "veth*"];
+  };
+
   console.keyMap = "es";
 
   users.users.luisnquin = {
