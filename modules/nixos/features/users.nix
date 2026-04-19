24ceefdc9c6fb73a0319baa432b909774577cee0
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-19 18:44:01 -0500

    fix: set initial user passwords

--- modules/nixos/features/users.nix
+++ modules/nixos/features/users.nix
@@ -1,11 +1,16 @@
 {...}: {
   flake.modules.nixos.users = {
-    users.users.luisnquin = {
-      isNormalUser = true;
-      extraGroups = ["wheel" "networkmanager" "video"];
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXW6vsDRgI/AiOdGnQOTyiz1uLFL0o66u0Ahcw9VWyd luis@quinones.pro"
-      ];
+    users.users = {
+      root.hashedPassword = "$6$djOTUxEpFIXIRtd3$RfBMFRLcgsaAWSRg.sHCQv87WRX3jE0gRpinMJwpu7cBk1HT2EaOzhGC829jlRVV7v4lMvMO99xmzAJWL/Exg.";
+
+      luisnquin = {
+        isNormalUser = true;
+        hashedPassword = "$6$djOTUxEpFIXIRtd3$RfBMFRLcgsaAWSRg.sHCQv87WRX3jE0gRpinMJwpu7cBk1HT2EaOzhGC829jlRVV7v4lMvMO99xmzAJWL/Exg.";
+        extraGroups = ["wheel" "networkmanager" "video"];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXW6vsDRgI/AiOdGnQOTyiz1uLFL0o66u0Ahcw9VWyd luis@quinones.pro"
+        ];
+      };
     };
   };
 }
