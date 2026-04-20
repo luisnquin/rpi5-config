56e2adaf80a58dee942abe170b69ff8e1cf1adb2
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-19 19:19:56 -0500

    feat(users/luisnquin): add new trusted pubkey

--- modules/nixos/features/users.nix
+++ modules/nixos/features/users.nix
@@ -9,6 +9,7 @@
         extraGroups = ["wheel" "networkmanager" "video"];
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXW6vsDRgI/AiOdGnQOTyiz1uLFL0o66u0Ahcw9VWyd luis@quinones.pro"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICOvNB4XZFchiWUCpdXaNcyoyUi9+7SnGCvrRk2CM129"
         ];
       };
     };
